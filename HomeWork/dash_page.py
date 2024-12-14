81a727eb1efcb250dccfb7fa556fb15847a033bc
Austin <austin.chang16@gmail.com>
2024-12-14 19:15:17 +0800

    success combine the flask and dash server

--- HomeWork/dash_page.py
+++ HomeWork/dash_page.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/Austin-Chang-zz/Python_Web/main/HomeWork/sales_orders.csv')
 
-app = Dash(__name__)
+app1 = Dash(__name__,requests_pathname_prefix="/dash/")
 
 
 # Define the layout
-app.layout = html.Div([
+app1.layout = html.Div([
     html.H1("Production Page", style={'textAlign': 'center'}),
     dcc.Markdown("### Welcome to the Production Page"),
     dcc.Markdown(f"**Data Preview:**\n\n{df.head().to_markdown()}")
 ])
 if __name__ == '__main__':
-    app.run(debug=True)
+    app1.run(debug=True)
