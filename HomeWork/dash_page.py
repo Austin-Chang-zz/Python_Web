ca2a7185c1d6d6f5f1f2a9bda90ff9b31daddb74
Austin <austin.chang16@gmail.com>
2024-12-15 11:34:24 +0800

    add dropdown and plot chart

--- HomeWork/dash_page.py
+++ HomeWork/dash_page.py
@@ -1,5 +1,6 @@
-from dash import Dash,html,dcc
+from dash import Dash,html,dcc,callback,Input,Output
 import pandas as pd
+import plotly.express as px
 
 df = pd.read_csv('https://raw.githubusercontent.com/Austin-Chang-zz/Python_Web/main/HomeWork/sales_orders.csv')
 
@@ -10,7 +11,21 @@ app1 = Dash(__name__,requests_pathname_prefix="/dash/")
 app1.layout = html.Div([
     html.H1("Production Page", style={'textAlign': 'center'}),
     dcc.Markdown("### Welcome to the Production Page"),
-    dcc.Markdown(f"**Data Preview:**\n\n{df.head().to_markdown()}")
+    dcc.Markdown(f"**Data Preview:**\n\n{df.head().to_markdown()}"),
+    dcc.Dropdown(df.sales_name.unique(),value="Alice",id='dropdown-selection'),
+    dcc.Graph(id='graph-content') 
 ])
+
+
+@callback(
+    Output('graph-content','figure'),
+    Input('dropdown-selection','value')
+)
+
+def update_graph(value):
+    dff = df[df.sales_name == value]
+    # return px.line(dff, x='order_date',y='yield_rate',title='Production Yield Rate')
+    return px.scatter(dff, x='order_date',y='yield_rate',title='Production Yield Rate')
+
 if __name__ == '__main__':
     app1.run(debug=True)
